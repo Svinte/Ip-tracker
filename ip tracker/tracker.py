92f40033494647115ebccfc3f7db98e6a41c7cbf
Svinte <99759858+Svinte@users.noreply.github.com>
2024-01-24 15:51:24 +0200

    IP tracker 1.1
    What's new?
    -setup arguments
    -custom domain
    -debug off mode

---
+++ ip tracker/tracker.py
@@ -0,0 +1,42 @@
+# Import libraries
+from flask import Flask, request
+from datetime import datetime
+import json
+import argparse
+
+# Arguments
+argument_parser = argparse.ArgumentParser(description="Betupe is IP fishing tool. You can set up server that logs all IP addresses from requests.")
+argument_parser.add_argument("--port", "-p", type=int, help="Add custom port for server")
+argument_parser.add_argument("--domain", "-d", type=str, help="Add server domain to get acces to internet")
+argument_parser.add_argument("--no_debug", "-nD", action="store_true", help="Turn debugging off")
+arguments = argument_parser.parse_args()
+
+# Flask app
+server = Flask(__name__)
+
+# Server port
+file = open("setup.json")
+json_data = json.load(file)
+port =  arguments.port or json_data["port"] or 4400
+domain = arguments.domain or json_data["domain"] or "127.0.0.1"
+debug = arguments.no_debug or not json_data["no_debug"] or False
+
+# GET-request handler
+@server.route("/")
+def get_request():
+    time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    file = open("data/"+str(time), "w")
+    json.dump(
+        {
+            "time": time,
+            "type": request.method,
+            "ip_addr": request.remote_addr
+        },
+        file
+    )
+    file.close()
+    page = open("index.html", "r")
+    return page
+
+if __name__ == "__main__":
+    server.run(port=port, host=domain, debug=debug)
