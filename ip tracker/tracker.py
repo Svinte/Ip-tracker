6c977b4c999c30d479af3c9deb848222c743270e
Svinte <99759858+Svinte@users.noreply.github.com>
2024-01-26 19:43:24 +0200

    Ip tracker 1.2
    - custom file path via arguments

--- ip tracker/tracker.py
+++ ip tracker/tracker.py
@@ -9,17 +9,19 @@ argument_parser = argparse.ArgumentParser(description="Betupe is IP fishing tool
 argument_parser.add_argument("--port", "-p", type=int, help="Add custom port for server")
 argument_parser.add_argument("--domain", "-d", type=str, help="Add server domain to get acces to internet")
 argument_parser.add_argument("--no_debug", "-nD", action="store_true", help="Turn debugging off")
+argument_parser.add_argument("--path", "-fp", type=str, help="Server response file path.")
 arguments = argument_parser.parse_args()
 
 # Flask app
 server = Flask(__name__)
 
-# Server port
+# Server values
 file = open("setup.json")
 json_data = json.load(file)
 port =  arguments.port or json_data["port"] or 4400
 domain = arguments.domain or json_data["domain"] or "127.0.0.1"
 debug = arguments.no_debug or not json_data["no_debug"] or False
+return_file = arguments.path or not json_data["return_file"] or "index.html"
 
 # GET-request handler
 @server.route("/")
@@ -35,7 +37,7 @@ def get_request():
         file
     )
     file.close()
-    page = open("index.html", "r")
+    page = open(return_file, "r")
     return page
 
 if __name__ == "__main__":
